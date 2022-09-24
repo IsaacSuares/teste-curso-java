4265fe7046a9ee922cfd66758f09a6b2835b28dc
Isaac <isaacgames95@gmail.com>
2022-09-23 23:17:32 -0300

    first commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+import pastaGato.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 3;
+		Gato gato = new Gato();
+		System.out.println(gato);
+	}
+}
---
+++ src/pastaGato/Gato.java
@@ -0,0 +1,9 @@
+package pastaGato;
+
+public class Gato {
+
+	public String nome;
+	public String cor;
+	public Integer idade;
+	
+}
